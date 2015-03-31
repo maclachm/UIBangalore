9dade57d42d6c9fdc21f08c6a7f32433ac01cb27
maclachm <maclachm@gmail.com>
2015-03-30 22:54:55 -0400

    uikit added

--- UIBangalore/routes/index.js
+++ UIBangalore/routes/index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
+router.use(express.static(__dirname + '/public'));
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'UI Bangalore' });
