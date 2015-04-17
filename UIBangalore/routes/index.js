89242e0e7bf867b9b1abb451186df1d8fcc26fa1
maclachm <maclachm@gmail.com>
2015-04-17 08:27:41 -0400

    update

--- UIBangalore/routes/index.js
+++ UIBangalore/routes/index.js
@@ -7,31 +7,59 @@ router.use(express.static(__dirname + '/public'));
 router.use(bodyParser.json()); // for parsing application/json
 router.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
+var googleApiKey = "AIzaSyBRiLzoPGoUOXwd0UFh-ivscYbEfoHVi8g"
+
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index', { title: 'UI Bangalore' });
 });
 
-router.get('/map', function(req, res) {
-  res.render('map', { title: 'UI Bangalore' });
-});
 
-router.get('/data', function(req, res, next) {
-  res.render('map', { title: 'UI Bangalore' });
-  next();
-});
+var mapInit = function(req, res, attrObject) {
 
-router.get('/data', function(req, res) {
+    res.render('map', attrObject);
+}
+
+//router.get('/data', [dbRet, mapInit]);
+
+router.get('/data', function(req, res){
+
+    mapAttributes = {} //jade attributes for map
+
+    mapAttributes.title = req.query.name;
+    mapAttributes.basePoint = [42, -83];
+    //mapAttributes.layer = 
+    //dbReq(mapAttributes.title, req, mapAttributes); 
+
+
+    queryName = mapAttributes.title.replace(/['"\\;{}]$/g,'') // strip misc/malicious chars
+    queryName = queryName.replace(/([b-d|f-h|j-n|p-t|v-z]){1,2}/g, '$1'+'+'); // regex to fix minor spelling inconsistencies/repeat chars
+
+
     var db = req.db;
+    db.get('features').find( {"properties.Name" : {$regex: queryName }},  function (err, items) {
+        if (err)
+            return err
 
+        //return items
+        mapAttributes.featureObject = JSON.stringify(items);
 
-    var queryName = req.query.name;
+        db.get('guidanceValues').find( {"VILLAGE/AREA/ROAD" : {$regex: queryName }},  function (err, values) {
+        if (err)
+            return err
 
-    queryName = queryName.replace(/['"\\;{}]$/g,'') // strip misc/malicious chars
+        //return values
+        mapAttributes.guidanceObject = JSON.stringify(values);
 
-    db.get('features').find( {"properties.Name" : {$eq: name }},  function (err, items) {
-        res.json(items);
+        //NB put in a different format, buffer, etc
+
+        res.render('map', mapAttributes);
+        }); 
+
     }); 
+
+
+    //res.render('map', {title: value, basePoint: [42, -83], object: layer });
 });
 
 
--- UIBangalore/routes/map.js
+++ UIBangalore/routes/map.js
@@ -1,5 +1,13 @@
 var express = require('express');
 var router = express.Router();
+var bodyParser = require('body-parser');
+var queryName;
+
+router.use(express.static(__dirname + '/public'));
+
+router.use(bodyParser.json()); // for parsing application/json
+router.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+
 
 /*
 router.get('/data', function(req, res) {
