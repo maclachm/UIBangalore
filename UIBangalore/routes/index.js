4ddc2f1d988072c4e3fecaf9d9a995f743237646
maclachm <maclachm@gmail.com>
2015-04-17 10:53:04 -0400

    working alpha (?)

--- UIBangalore/routes/index.js
+++ UIBangalore/routes/index.js
@@ -26,25 +26,25 @@ router.get('/data', function(req, res){
 
     mapAttributes = {} //jade attributes for map
 
-    mapAttributes.title = req.query.name;
+    mapAttributes.title = req.query.name.replace(/^\s+|\s+$/g, '');
     mapAttributes.basePoint = [42, -83];
     //mapAttributes.layer = 
     //dbReq(mapAttributes.title, req, mapAttributes); 
 
 
     queryName = mapAttributes.title.replace(/['"\\;{}]$/g,'') // strip misc/malicious chars
-    queryName = queryName.replace(/([b-d|f-h|j-n|p-t|v-z]){1,2}/g, '$1'+'+'); // regex to fix minor spelling inconsistencies/repeat chars
+    queryRegex = queryName.replace(/([^aeiou\s\b]){1}/g, '$1'+'+'); // regex to fix minor spelling inconsistencies/repeat chars
 
-
+    //{$or: [{"properties.Name": {$regex: 'G'}}, {"properties.Name":{$eq: 'G'}}]})
     var db = req.db;
-    db.get('features').find( {"properties.Name" : {$regex: queryName }},  function (err, items) {
+    db.get('features').find( {$or :[{"properties.Name" : {$eq: queryName }}, {"properties.Name" : {$regex: queryRegex }} ]},  function (err, items) {
         if (err)
             return err
 
         //return items
         mapAttributes.featureObject = JSON.stringify(items);
 
-        db.get('guidanceValues').find( {"VILLAGE/AREA/ROAD" : {$regex: queryName }},  function (err, values) {
+        db.get('guidanceValues').find( {$or :[{"VILLAGE/AREA/ROAD" : {$eq: queryName }}, {"VILLAGE/AREA/ROAD" : {$regex: queryRegex }} ]},  function (err, values) {
         if (err)
             return err
 
